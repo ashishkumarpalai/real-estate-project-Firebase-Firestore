ba38ac6139945300ea8af0e160c322d6fc82e0b4
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2024-09-27 01:23:27 +0530

    version 1 complete

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,8 +1,23 @@
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../serviceAccountKey.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// const db = admin.firestore();
+// module.exports = db;
+
+
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+require('dotenv').config();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 const db = admin.firestore();
